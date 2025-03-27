84eb1fbfa96ddd72d73672dce7300cf41bbff726
Albert Barber <albert.barber1@gmail.com>
2025-03-27 14:53:10 -0700

    Updated Code and Images

--- Code/Wall_Spiral_Code/Wall_Spiral_Code.ino
+++ Code/Wall_Spiral_Code/Wall_Spiral_Code.ino
@@ -1,13 +1,13 @@
 #define FASTLED_INTERNAL 1  //turns of the pragma messages from FastLED
 #include <Pixel_Spork.h>
-#include "effectStore.h"
 
 //FastLED vars
-#define NUM_LEDS 90
+#define NUM_LEDS 90 //total number of LEDs
 #define DATA_PIN D8
 
 //Effect count
-#define NUM_EFFECTS 22
+//See bottom of code for spare effects
+#define NUM_EFFECTS 23
 
 //===============================================================
 //WARNING, must be compiled using ESP8266 Version 2.7.4 or lower,
@@ -20,7 +20,7 @@
 #define LIGHTSENPIN A0
 #define LIGHTSEN_ENABLE_PIN D5  //jumper block reading pin, used to enable / disable the light sensor reading at boot
 #define LIGHTSEN_THRESHOLD 1000
-boolean lightSenDisable = false;
+boolean lightSenDisable = false; //if true, then the light sensor will be ignored 
 
 // Define the array of leds for FastLED
 CRGB leds[NUM_LEDS];
@@ -35,7 +35,8 @@ CRGB leds[NUM_LEDS];
 //                          Palette Classes Setups
 //========================================================================
 
-//Random palettes
+//Random palettes 
+//(initialized with red, green, and blue starting colors, they will be randomized during setup())
 CRGB rand3Pallet_Arr[] = {CRGB::Red, CRGB::Blue, CRGB::Green};
 palettePS rand3Pallet1 = {rand3Pallet_Arr, SIZE(rand3Pallet_Arr)};
 
@@ -56,12 +57,6 @@ palettePS rand2Pallet3 = {rand2Pallet_Arr3, SIZE(rand2Pallet_Arr3)};
 
 CRGB singPal_Arr[] = {CRGB::Red};
 palettePS singPal = {singPal_Arr, SIZE(singPal_Arr)};
-
-//========================================================================
-//                           Patterns
-//========================================================================
-//uint8_t christStreamPat_arr = {0, 0, 0, 255, 255, 255, 255, 255};
-//patternPS christStreamPat = {christStreamPat_arr, SIZE(christStreamPat_arr)};
 
 //========================================================================
 //                          Util Classes Setups
@@ -89,54 +84,28 @@ palettePS &randPB2Pal = Rand2PB.blendPalette;
 
 //RateNoisePS rateNoise(10, 40, 10, 80);
 
-//===================================================================================
+//========================================================================
+//                          Effect Names
+//========================================================================
 ColorModeFillPS *ColorFill;
-AddGlitterPS *Glitter;
-//CrossFadeCyclePS *CFC;
-ShiftingSeaSL *seaSft;
-TwinkleSL *Twinkl;
-//TwinkleFastSL *CFC;
+//AddGlitterPS *Glitter;
+//ShiftingSeaSL *seaSft;
 Twinkle2SLSeg *Twinkl2;
-FairyLightsSLSeg *FL;
-//DissolveSL *Dis;
-//SoftTwinkleSL *CFC;
-//GlimmerSL *Glim;
-ShimmerSL *Shim;
-PrideWPalSL *Pride;
 PrideWPalSL2 *Pride2;
-//TheaterChaseSL *CFC;
-GradientCycleSL *GC;
 GradientCycleFastSL *GCF;
 StreamerSL *Strem;
-//StreamerFastSL *CFC;
 RollingWavesSL *Roll;
 RollingWavesFastSL *RollFa;
 ParticlesSL *Parti;
-//LarsonScannerSL *CFC;
-//PoliceStrobeSL *CFC;
-//StrobeSeg *CFC;
-//Fire2012Seg *Fire;
-Fire2012SL *Fire;
 RainSL *rainSL;
 RainSeg *rainSeg;
-//PacificaPS *CFC;
-PacificaHueSL *PaciH;
+//PacificaHueSL *PaciH;
 LavaPS *Lav;
-//PlasmaSL *CFC;
-//Noise16PS *CFC;
 NoiseSL *Noise;
-FireworksPS *FW;
-FirefliesSL *FF;
-//NoiseWavesSL *CFC;
-NoiseGradSL *NoiseGrad;
+//NoiseGradSL *NoiseGrad;
 EdgeBurstSL *Edge;
-ColorMeltSL *ColMelt;
-//RainbowFontsSL *RainFont;
+//ColorMeltSL *ColMelt;
 SegWaves *Waves;
-//SegWavesFast *WavesFast;
-//ScannerSL *Scan;
-//BreathPS *CFC;
-//BreathEyeSL *CFC;
 ColorWipeSLSeg *ColorWipe;
 
 //=====================================================================================
@@ -195,6 +164,26 @@ void setup() {
 
     lotusSegs.runOffset = true;
 }
+
+//-------------------Main Loop Code--------------------------
+//-----------------------------------------------------------
+//To remove an effect, simply set its effectSet.runTime = 0 and it will be skipped over
+
+//To add a new effect, add the following code block to the large switch statement below,
+//filling in the case number, effect constructor, and effect name.
+//If using a new type of effect you must add it in the "effect names" section above.
+//You'll also need to increment NUM_EFFECTS above.
+/* 
+case <<case number>>: {
+    if(!def) {
+        <<effect constructor>>
+        effectSet.runTime = 20000; //runtime in ms
+        effectSet.setEffect(<<effect name>>, 2);
+    } else {
+        //
+    }
+} break;
+*/
 
 //Each loop, we're either updating and cycling effects,
 //or checking the light level to start an effect
@@ -427,6 +416,15 @@ void loop() {
                     //
                 }
             } break;
+            case 22: {
+                if(!def) {
+                    ColorFill = new ColorModeFillPS(CWspiralSegs, 2, 60);
+                    effectSet.runTime = 10000;
+                    effectSet.setEffect(ColorFill, 2);
+                } else {
+                    //
+                }
+            } break;
         }
 
         //We use "def" to track if an effect has been created
@@ -500,16 +498,11 @@ void resetLoopSettings() {
 }
 
 //--------------------------------------------------
-//       Effects Depot (contains used and spare effects)
+//     Effects Depot For Unused Effects
 //---------------------------------------------------
 
 //Particles
 //--------------
-//Parti = new ParticlesSL(CWspiralLineSeg, randPB3Pal, 0, 4, 2, 40, 40, 1, 0, 1, 9, 18, 2, randPB3Pal.length, true);
-//Parti->colorMode = 1;
-
-//Parti = new ParticlesSL(CWspiralLineSeg, randPB3Pal, 0, 7, 2, 70, 50, 1, 0, 1, 2, 5, 2, randPB3Pal.length, true);
-
 //Parti = new ParticlesSL(joinedRingSegments, randPB3Pal, 0, 3, 2, 80, 50, 1, 0, 2, 0, 2, 2, randPB3Pal.length, true);
 //Parti->colorMode = 3;
 //effectSet.runTime = 15000;  //20000
@@ -520,16 +513,6 @@ void resetLoopSettings() {
 //Parti = new ParticlesSL(CWspiralLineSeg, randPB3Pal, 0, 6, 2, 60, 30, 1, 0, 0, 0, 0, 2, randPB3Pal.length, true);
 //particleUtilsPS::setAllTrailRand(*Parti->particleSet, true, false, false, false, true);
 //rainSeg = new RainSeg(CCWspiralSegs, randPB3Pal, 0, true, 10, 2, 1, 0, 1, 3, true, false, false, false, true, 80, 50);
-//rainSL = new RainSL(joinedRingSegments, randPB3Pal, 0, true, 10, 1, 1, 0, 1, 3, false, true, false, false, true, 70, 50, false);
-
-//Rains
-//--------------
-//rainSeg = new RainSeg(CCWspiralSegs, randPB3Pal, 0, true, 10, 2, 1, 0, 1, 3, true, false, false, false, false, 80, 50);
-
-//rainSeg = new RainSeg(CCWspiralSegs, CRGB::White, 0, false, 10, 2, 1, 0, 1, 3, true, false, false, false, false, 80, 50);
-//rainSeg->bgColorMode = 3;
-//effectSet.runTime = 15000;  //20000
-//effectSet.setEffect(rainSeg, 0);
 
 //Pacifica
 //--------------
@@ -537,17 +520,9 @@ void resetLoopSettings() {
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(PaciH, 0);
 
-//Edge Burst
-//--------------
-//Edge = new EdgeBurstSL(joinedRingSegments, 0, true, true, 15, 80);
-//Edge = new EdgeBurstSL(CWspiralSegs, 2, true, true, 20, 100);
-//effectSet.runTime = 15000;  //20000
-//effectSet.setEffect(Edge, 0);
-
 //Streamer
 //--------------
 //Strem = new StreamerSL(CCWspiralLineSeg, randPB2Pal, 0, 9, 0, 10, 20);
-//Strem = new StreamerSL(CCWspiralLineSeg, randPB3Pal, 0, 9, 9, 10, 20);
 //Strem = new StreamerSL(joinedRingSegments, CRGB::White, 0, 2, 3, 10, 20);
 //Strem->bgColorMode = 3;
 
@@ -565,9 +540,7 @@ void resetLoopSettings() {
 
 //Lava
 //--------------
-//Lav = new LavaPS(CWspiralLineSeg, 20, 50, 50);
-//Lav = new LavaPS(CWspiralLineSeg, randPB3Pal, 20, 30, 50);
-//Lav->hueCycle = true;
+//Lav = new LavaPS(CWspiralLineSeg, 20, 50, 50); //default fire colors
 
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(Lav, 0);
@@ -581,30 +554,16 @@ void resetLoopSettings() {
 //Waves = new SegWaves(CWspiralSegs, randPB2Pal, 0, 5, 0, 5, false, 50); //add glitter?
 //Waves->randMode = 1;
 
-//Waves = new SegWaves(lotusSegs, randPB2Pal, 0, 1, 2, 10, false, 65);
-//Waves->colorMode = 4;
-
 //Waves = new SegWaves(lotusSegs, randPB2Pal, 0, 5, 2, 10, false, 65); //or 0 spacing
 //Waves->randMode = 1;
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(Waves, 0);
 
-//Pride
-//--------------
-//Pride2 = new PrideWPalSL2(joinedRingSegments, true, true, 80);
-//effectSet.runTime = 15000;  //20000
-//effectSet.setEffect(Pride2, 0);
-
 //Rolling Waves
 //--------------
-//Roll = new RollingWavesSL(mainSegments, randPB3Pal, 0, 5, 1, 2, 120);
-//Roll = new RollingWavesSL(joinedRingSegments, 2, 0, 5, 1, 0, 130); //<meh
+//Roll = new RollingWavesSL(joinedRingSegments, 2, 0, 5, 1, 0, 130);
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(Roll, 0);
-
-//RollFa = new RollingWavesFastSL(CWspiralSegs, 3, 0, 9, 1, 0, 130);
-//RollFa->randMode = 1;
-//effectSet.setEffect(RollFa, 0);
 
 //Shifting Sea
 //--------------
@@ -617,16 +576,14 @@ void resetLoopSettings() {
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(seaSft, 0);
 
-//Noise
+//Noise and Noise Gradient
 //--------------
 //NoiseGrad = new NoiseGradSL(halfRingSegments, randPB3Pal, 0, 3, 7, 20, 40, 40, 5000, 80);
 //NoiseGrad->doBrightness = false;
-
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(NoiseGrad, 0);
 
 //Noise = new NoiseSL(CWringSegments, randPB3Pal, 20, 30, 40, 10, 0, 80);
-//Noise = new NoiseSL(joinedRingSegments, randPB3Pal, 80, 30, 40, 10, 1, 80); //20 steps for mode 0
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(Noise, 0);
 
@@ -635,21 +592,12 @@ void resetLoopSettings() {
 //GCF = new GradientCycleFastSL(halfRingSegments, randPB3Pal, 5, 110);
 //GCF->randMode = 1;
 
-//GCF = new GradientCycleFastSL(CWringSegments, randPB3Pal, 6, 100);
-//GCF->randMode = 1;
-
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(GCF, 0);
 
 //Twinkle2
 //--------------
-//Twinkl2 = new Twinkle2SLSeg(halfRingSegments, randPB3Pal, 0, 5, 500, 5, 5, 5, 5, 0, 70);
-//Twinkl2->colorMode = 2;
-
 //Twinkl2 = new Twinkle2SLSeg(joinedRingSegments, randPB3Pal, 0, 10, 500, 2, 5, 5, 5, 0, 70);
-//Twinkl2->colorMode = 3;
-
-//Twinkl2 = new Twinkle2SLSeg(joinedRingSegments, randPB3Pal, 0, 25, 300, 2, 6, 2, 6, 2, 60);
 //Twinkl2->colorMode = 3;
 
 //effectSet.runTime = 15000;  //20000
@@ -657,9 +605,6 @@ void resetLoopSettings() {
 
 //Color Wipe
 //--------------
-//ColorWipe = new ColorWipeSLSeg(ringSegments, rand3Pallet3, 9, 2, true, false, false, true, 140);
-//ColorWipe->setUpLoop(true, 2, false, 1, true, false, true, false, false);
-
 //ColorWipe = new ColorWipeSLSeg(ringSegments, rand3Pallet3, 10, 2, true, false, false, true, 140);
 //ColorWipe->setUpLoop(true, 2, true, 1, true, false, true, false, false);
 
@@ -671,15 +616,6 @@ void resetLoopSettings() {
 //paletteUtilsPS::randomize(singPal);
 //}
 
-//ColorWipe = new ColorWipeSLSeg(lotusSegs, rand2Pallet3, 10, 2, true, false, true, true, 140);
-//ColorWipe->setUpLoop(true, 2, false, 1, true, false, true, false, false);
-
-//ColorWipe = new ColorWipeSLSeg(CWringSegments, singPal, 10, 0, true, false, true, false, 140);
-//ColorWipe->setUpLoop(true, 2, false, 1, true, false, true, false, false);
-//if(ColorWipe->loopCount != prevCycleNum){
-//prevCycleNum = ColorWipe->loopCount;
-//paletteUtilsPS::randomize(singPal);
-//}
 
 //effectSet.runTime = 15000;  //20000
 //effectSet.setEffect(ColorWipe, 0);
@@ -687,6 +623,7 @@ void resetLoopSettings() {
 //Extra
 //--------------
 //Glitter = new AddGlitterPS(joinedRingSegments, CRGB::White, 10, 1, 50, 50);
+
 //&randRate2.outputRate;
 //Waves->rate = &rateNoise.outputRate;
 
